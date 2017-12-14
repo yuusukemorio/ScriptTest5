afba93300ded319e927caea058c50e49d0acc200
yuusukemorio <Morimori2003@gmail.com>
2017-12-14 18:11:28 +0900

    ScriptTest　「C#スプリクトの基礎」の課題です

---
+++ Assets/A.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        int[] array = new int[5];
+
+        array [0] = 10;
+        array [1] = 20;
+        array [2] = 30;
+        array [3] = 40;
+        array [4] = 50;
+
+        for (int R = 0; R < 5; R++) {
+            Debug.Log(array[R]);
+        }
+    }
+
+	
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss
+{
+    private int hp = 100;  // 体力
+    private int mp = 53;   //魔力
+    private int power = 25; // 攻撃力
+
+    // 攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log("打撃で" + this.power + "のダメージを与えた");
+    }
+
+    // 防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+
+    //魔法用の関数
+    public void Magic(int Magic)
+    {
+
+        if (mp >= 5)
+        {
+            Debug.Log("魔法で" + Magic + "のダメージを与えた");
+        //残りmpを減らす
+        this.mp -= Magic;
+        Debug.Log(mp);
+
+        }
+
+        if (mp <= 5)
+        {
+            Debug.Log("MPが足りないみたいですよ");
+        }
+
+    }
+
+}
+
+public class Test : MonoBehaviour
+{
+
+    void Start()
+    {
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        // 攻撃用の関数を呼び出す
+        lastboss.Attack();
+        // 防御用の関数を呼び出す
+        lastboss.Defence(3);
+        // 魔法用の関数を呼び出す
+
+        for (int v = 0; v < 10; v++) {
+            lastboss.Magic(5);
+
+                }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
